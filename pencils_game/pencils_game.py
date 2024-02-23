3cadc418b4e2548869fe8df1c636bc0d5c363101
Vakariev <v.i.vakariev@student.khai.edu>
2024-02-23 21:04:22 +0200

    Pencils 4th stage

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -14,9 +14,9 @@ def get_number_of_pencils():
 
 def get_first_player():
     while True:
-        first_player = input("Who will be the first (John, Jack):\n")
-        if first_player.lower() not in ['john', 'jack']:
-            print("Choose between 'John' and 'Jack'")
+        first_player = input("Who will be the first player (John or Jack)?\n").capitalize()
+        if first_player not in ['John', 'Jack']:
+            print("Please choose between 'John' and 'Jack'")
         else:
             return first_player
 
@@ -24,7 +24,13 @@ def get_first_player():
 def take_pencils(current_player, remaining_pencils):
     while True:
         try:
-            num_taken = int(input(f"{current_player}'s turn:\n"))
+            if current_player == 'John':
+                num_taken = int(input(f"{current_player}'s turn:\n"))
+            else:  # Jack's turn
+                if remaining_pencils % 4 == 0:
+                    num_taken = random.randint(1, 3)
+                else:
+                    num_taken = remaining_pencils % 4
             if num_taken < 1 or num_taken > 3:
                 print("Possible values: '1', '2' or '3'")
             elif num_taken > remaining_pencils:
@@ -35,21 +41,38 @@ def take_pencils(current_player, remaining_pencils):
             print("Possible values: '1', '2' or '3'")
 
 
+def bot_turn(remaining_pencils):
+    if remaining_pencils % 4 == 0:
+        return random.randint(1, 3)
+    else:
+        return remaining_pencils % 4
+
+
 def main():
     num_pencils = get_number_of_pencils()
-    current_player = get_first_player()
+
+    first_player = get_first_player()
+    current_player = first_player
 
     while num_pencils > 0:
         print("|" * num_pencils)
 
-        num_taken = take_pencils(current_player, num_pencils)
-        num_pencils -= num_taken
-
-        if num_pencils <= 0:
-            break
-
-        # Зміна поточного гравця
-        current_player = 'Jack' if current_player == 'John' else 'John'
+        if current_player == 'John':
+            num_taken = take_pencils('John', num_pencils)
+            num_pencils -= num_taken
+            if num_pencils <= 0:
+                break
+            current_player = 'Jack'
+        else:
+            if num_pencils <= 3:
+                num_taken = num_pencils
+            else:
+                num_taken = bot_turn(num_pencils)
+            num_pencils -= num_taken
+            if num_pencils <= 0:
+                break
+            print(f"Jack's turn: {num_taken}")
+            current_player = 'John'
 
     winner = 'John' if current_player == 'Jack' else 'Jack'
     print(f"{winner} won!")
